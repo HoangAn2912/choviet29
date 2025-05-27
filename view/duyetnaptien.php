4ca7726cd8543f2c73f6e6be95a61e3384bc5e61
code <hoangan2912.npha@gmail.com>
2025-05-27 09:50:06 +0700

    up lan cuoi

--- view/duyetnaptien.php
+++ view/duyetnaptien.php
@@ -74,7 +74,6 @@ try {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Quản lý giao dịch nạp tiền</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="/project/css/duyetnaptien.css">
 </head>
